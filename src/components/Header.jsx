59ef856109bfe8f904606b877be0cfe3774a4eb1
musabolug <musabolug@gmail.com>
2023-02-28 16:35:34 +0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div style={{color: "ghostwhite" ,fontSize: "70px"}}>
+       Designed Quote Generator
+    </div>
+  )
+}
+
+export default Header
